d6c9dc3190811c1c5ef7ab5cb6ddb082e49cacba
Brian Smith <brian.bubbam@gmail.com>
2021-09-07 09:48:00 -0500

    added the faker gem to create users in the seed file

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,11 +2,15 @@ class ProfilesController < ApplicationController
 
     #update profile
     def update
-        user = User.find_by(id: session[:id])
-        if user
-            user.update(profile_params)
+        user = User.find_by(id: session[:user_id])
+        profile = user.profile
+        #byebug
+        if profile
+            profile.update(profile_params)
             render json: user, status: 200
             #update user
+        elsif !user
+            render json: { errors: user.errors.full_messages }, status: :unauthorized
         else
             render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
@@ -15,6 +19,6 @@ class ProfilesController < ApplicationController
     private
 
     def profile_params
-        params.require(:user).permit(:image, :bio, :status)
+        params.require(:profile).permit(:image, :bio, :status)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,15 @@ User.destroy_all
 Profile.destroy_all
 
 a = User.create(username: "admin", password: "admin", password_confirmation: "admin")
+b = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
+c = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
+d = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
+e = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
+f = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
+g = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
+h = User.create(username: Faker::Superhero.name, password: "admin", password_confirmation: "admin")
 
-Profile.create(user_id: a.id, image: "" , bio: "test bio", status: "test status")
+#dont need to make a profile now because every time a user is created a profile is created for them automatically
+#Profile.create(user_id: a.id, image: "" , bio: "test bio", status: "test status")
 
 puts "seed done"
